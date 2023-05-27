f0a2ebc2987431fa0fb5094e6e50e8e8700e365d
lorddesert <sacha.nocetto@hotmail.com>
2023-05-27 16:22:55 -0300

    feat: add search form and show initial data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-						<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-						<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-					</ul>
-				</div>
-			</div>
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton';
+	export let data;
+
+	function handleChange(e: any) {
+		console.log(e.target.value);
+	}
+</script>
+
+<AppBar>
+	<form on:submit={handleChange} class="flex gap-3">
+		<input class="input" type="search" name="offers" id="offers" on:change={handleChange} />
+		<button class="btn variant-filled-secondary">Buscar ofertas</button>
+	</form>
+</AppBar>
+
+<div class="container h-full mx-auto flex">
+	<ul>
+		{JSON.stringify(data)}
+	</ul>
+</div>
