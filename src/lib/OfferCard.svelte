b6b88e852ffcc6732395cf098d29a5e55629c4f1
lorddesert <sacha.nocetto@hotmail.com>
2023-05-27 15:30:43 -0300

    feat: add component

---
+++ src/lib/OfferCard.svelte
@@ -0,0 +1,26 @@
+<script lang='ts'>
+  let title: string, company: string
+</script>
+
+
+<article>
+  <header>
+    <img src="" alt={company}>
+  </header>
+
+  <main>
+    <h2>
+      {title}
+    </h2>
+    <h3>
+      {company}
+    </h3>
+  </main>
+  <footer>
+    <small>location</small>
+    <span>divider</span>
+    <small>remote/hybrid/etc</small>
+    <span>divider</span>
+    <small>salary</small>
+  </footer>
+</article>
