3880cf6e0becd4c3d72b1a1a13c894de5dd5f21a
lorddesert <sacha.nocetto@hotmail.com>
2023-06-05 20:40:11 -0300

    feat: add component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+
+	import { AppBar } from "@skeletonlabs/skeleton";
+	import Logo from "./Logo.svelte";
+	import OfferSearchBar from "./OfferSearchBar.svelte";
+
+</script>
+<header>
+	<AppBar>
+		<div class="flex gap-6">
+			<Logo />
+			<!-- <OfferSearchBar getOffers={getOffers}/> -->
+			<OfferSearchBar />
+		</div>
+	</AppBar>
+</header>
