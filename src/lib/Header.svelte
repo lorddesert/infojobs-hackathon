b329708c23fe46e7625c164b02d02af7f5b0f361
lorddesert <sacha.nocetto@hotmail.com>
2023-06-14 19:53:08 -0300

    Change header to simulate header of info-jobs

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,16 +1,29 @@
 <script>
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import Logo from './Logo.svelte';
+	import OfferSearchBar from './OfferSearchBar.svelte';
 
-	import { AppBar } from "@skeletonlabs/skeleton";
-	import Logo from "./Logo.svelte";
-	import OfferSearchBar from "./OfferSearchBar.svelte";
-
+	/* 
+	background-color: #fff;
+margin: 0 auto;
+max-height: 80px;
+max-width: 1280px;
+	*/
+	/* 
+	
+	align-items: center;
+display: flex;
+padding: 0 16px;
+position: relative;
+	*/
 </script>
-<header>
-	<AppBar>
-		<div class="flex gap-6">
+
+<div class="bg-white">
+	<header class="mx-auto my-0 max-h-20 max-w-[1280px]">
+		<div class="flex items-center gap-7 px-4 relative h-[80px]">
 			<Logo />
 			<!-- <OfferSearchBar getOffers={getOffers}/> -->
 			<OfferSearchBar />
 		</div>
-	</AppBar>
-</header>
+	</header>
+</div>
