2063b62ae6b1acd782889a840197195b6a92e652
lorddesert <sacha.nocetto@hotmail.com>
2023-06-23 03:25:01 -0300

    WIP: aside form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,69 +2,31 @@
 	import Header from '$lib/Header.svelte';
 	import type { Item } from '$lib/types.js';
 	import { offerList as storedOffers } from '$lib/store';
-	import { Paginator } from '@skeletonlabs/skeleton';
+	import Aside from '$lib/Aside.svelte';
+	import Offer from '$lib/Offer.svelte';
 	export let data;
 	let offerList: Item[] = data.offerList;
-	let page = {
-		offset: 0,
-		limit: 20,
-		size: offerList.length,
-		amounts: [1, 2, 5, 10]
-	};
-
+	// let searchParam = ''
 	storedOffers.subscribe((newOffers) => {
 		if (newOffers.length) offerList = newOffers;
-
-		console.log(newOffers);
 	});
 </script>
 
 <Header />
 
-<main>
-	<div class="container h-full mx-auto flex mt-6">
+<h1 class="h5 mb-3">{offerList.length} resultados</h1>
+<div class="flex gap-3  max-w-[1280px] mx-auto my-0">
+	<Aside />
+	<main class="container h-full mx-auto flex mr-6">
+		<!-- <h2>{offerList.length} ofertas de {searchParam}</h2> -->
+		<div>
 			<ul>
 				{#each offerList as offer}
 					<li class="mb-4">
-						<a href={offer.link}>
-							<div class="card rounded-sm p-6 grid grid-cols-[100px_1fr]">
-								<aside class="m-auto">
-									<img
-										class=" w-20 rounded-md"
-										src={offer.author.logoUrl || 'fallback-company-logo.png'}
-										alt={offer.author.name}
-									/>
-								</aside>
-
-								<div>
-									<header>
-										<h3>
-											{offer.title}
-										</h3>
-										<a class="text-cyan-700 hover:underline" href={offer.author.uri}>
-											<h4>
-												{offer.author.name}
-											</h4>
-										</a>
-									</header>
-
-									<footer class=" flex gap-2 mt-4">
-										<small class="chip rounded-full variant-soft"
-											>{offer.teleworking?.value || 'No especifico'}</small
-										>
-										{#if offer.salaryMin.value}
-											<small class=" chip rounded-full variant-filled-success text-sm">
-												{offer.salaryMin.value}
-											</small>
-										{/if}
-									</footer>
-								</div>
-							</div>
-						</a>
+						<Offer {offer} />
 					</li>
 				{/each}
 			</ul>
-		<Paginator  bind:settings={page}>
-		</Paginator>
-	</div>
-</main>
+		</div>
+	</main>
+</div>
