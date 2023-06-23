1bd77d0bb112b3a6d7d2890ee49b2bd176f506cd
lorddesert <sacha.nocetto@hotmail.com>
2023-06-23 03:24:13 -0300

    feat: add store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,3 +1,22 @@
 import { writable } from 'svelte/store';
 
 export const offerList = writable([]);
+
+/**
+ * @param {string} query
+ */
+export async function  updateOffers(query) {
+  if (!query) {
+    return;
+  }
+
+  const response = await fetch(`/api/offer?q=${query}`);
+
+  const data = await response.json();
+
+  if (!response.ok) throw new Error(`${response.status}: ${response.statusText}`)
+
+  offerList.set(data.offers);
+
+  return true
+}
