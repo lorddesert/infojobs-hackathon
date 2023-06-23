615d3317a24d4adf35c4d9961b8989ef7637dbee
lorddesert <sacha.nocetto@hotmail.com>
2023-06-23 03:24:48 -0300

    feat: create component

---
+++ src/lib/CheckboxList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  type Data = {
+    count: number
+    name: string
+  }
+
+	export let data: Data[] = [];
+</script>
+
+<ul>
+	{#each data as  {name, count}}
+		<li>
+			<label for={`contract-type-${name}`}>
+				<input type="checkbox" name="contract-type" id={`contract-type-${name}`} />
+        {count}
+        {name}
+			</label>
+		</li>
+	{/each}
+</ul>
